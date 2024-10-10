8d17eadaae6d666abc79da421c111db9b6c55388
FARIZ DZULHAMI <dzulhamipablo@gmail.com>
2024-10-10 14:47:05 +0700

    Delete test1.py

--- test1.py
+++
@@ -1,9 +0,0 @@
-print("="*40)
-print("PROGRAM SEGITIGA")
-print("="*40)
-
-tinggi = float(input("Masukan tinggi :"))
-alas = float(input("Masukan alas : "))
-
-luas = alas * tinggi /2
-print("luas",luas,("cm2"))
