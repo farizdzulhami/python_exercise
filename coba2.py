fcbd36c600fe401770553315ed8c5abc537134c0
FARIZ DZULHAMI <dzulhamipablo@gmail.com>
2024-10-10 14:45:27 +0700

    Delete coba2.py

--- coba2.py
+++
@@ -1,9 +0,0 @@
-print('='*40)
-print('PROGRAM SEGITIGA')
-print('='*40)
-
-tinggi = float(input('Masukan Tinggi: '))
-alas = float(input('Masukan Alas'))
-
-luas = alas*tinggi /2
-print('luas',luas,('cm2'))
